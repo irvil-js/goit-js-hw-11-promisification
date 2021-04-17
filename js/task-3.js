5191a7c6b9c838fef8a1c375340aebcb44745c6b
Irina Murashka <irvil@tut.by>
2021-04-17 02:06:36 +0100

    исправила вывод времени

--- js/task-3.js
+++ js/task-3.js
@@ -21,28 +21,31 @@ const randomIntegerFromInterval = (min, max) => {
 // };
 
 const makeTransaction = (transaction) => {
-    return new Promise((resolve, reject) =>{
+    return new Promise((resolve, reject) => {
         const delay = randomIntegerFromInterval(200, 500);
   
   setTimeout(() => {
       const canProcess = Math.random() > 0.3;
 
-      if (canProcess) {
-        resolve(transaction.id, delay);
-      } else {
-        reject(transaction.id);
+     if (canProcess) {
+        resolve({
+          id: transaction.id,
+          time: delay,
+        });
       }
+      reject(transaction.id);
     }, delay);
   });
 };
 
-const logSuccess = (id, time) => {
-  console.log(`Transaction ${id} processed in ${time}ms`);
+const logSuccess = ({ id, time }) => {
+  console.log(`Transaction ${id} processed in ${time} ms`);
 };
 
 const logError = id => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
+
 
 // /*
 //  * Работает так
